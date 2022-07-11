64eb62514dd7cc614ce8c2520bc41b356c7a33f8
Vadim Belyaev <vadimbelyaev@gmail.com>
2022-07-11 14:05:17 +0400

    Create empty SwiftUI project

---
+++ StinsenNavigationIssueExample/StinsenNavigationIssueExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StinsenNavigationIssueExampleApp.swift
+//  StinsenNavigationIssueExample
+//
+//  Created by Vadim Belyaev on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct StinsenNavigationIssueExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
